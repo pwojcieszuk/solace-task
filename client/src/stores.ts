c1f92cb12ad97ac9ecfe23d26aee1d5f29954cbe
Piotr Wojcieszuk <pwojcieszuk@gmail.com>
2023-07-02 14:49:11 +0200

    Mock of store fetching data

--- client/src/stores.ts
+++ client/src/stores.ts
@@ -5,6 +5,41 @@ function createFavorites() {
         new Map<number, { title: string; image: string }>(),
     );
 
+    const fv = [
+        {
+            id: 51252,
+            title: "Spy Kyoushitsu",
+            image: "https://cdn.myanimelist.net/images/anime/1491/132864.webp",
+        },
+
+        {
+            id: 51815,
+            title: "Kubo-san wa Mob wo Yurusanai",
+            image: "https://cdn.myanimelist.net/images/anime/1818/132330.webp",
+        },
+
+        {
+            id: 24833,
+            title: "Ansatsu Kyoushitsu",
+            image: "https://cdn.myanimelist.net/images/anime/5/75639.webp",
+        },
+    ];
+
+    const fetchFavorites = async () => {
+        const data = await Promise.resolve(fv);
+
+        set(
+            new Map(
+                data.map((f: { id: number; title: string; image: string }) => [
+                    f.id,
+                    { title: f.title, image: f.image },
+                ]),
+            ),
+        );
+    };
+
+    fetchFavorites();
+
     return {
         subscribe,
         add: (mal_id: number, title: string, image: string) =>
